8188f91ee7186945b73e4ff7e661562c611d1e69
MoGracey <gmoyin53@gmail.com>
2023-07-17 08:03:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* * main - Entry points-Lower Alpha
+* * Return: Always (Success)
+*/
+
+int main(void)
+
+{
+	       char loweralpha;
+
+		for (loweralpha = 'a'; loweralpha <= 'z'; loweralpha++)
+			putchar(loweralpha);
+			putchar('\n');
+			return (0);
+}
