f331ef1da55756b6132376c0843fcc73cd0b9b6d
MoGracey <gmoyin53@gmail.com>
2023-08-03 06:08:46 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,18 @@
  *
  * Return: return factorial
  */
-
 int factorial(int n)
 {
-	if (n == 0)
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n < 2)
+	{
 		return (1);
+	}
 	else
+	{
 		return (factorial(n - 1) * n);
+	}
 }
