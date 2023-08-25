2a606847d33ab9c9690f47406f9b4c520abccfc0
MoGracey <gmoyin53@gmail.com>
2023-08-25 11:30:26 -0700

    elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,10 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 while (h != NULL)
 	{
-		if (h->str == NULL)
-		printf("[0] (nil)\n");
-		else
-		printf("[%lu] %s\n", strlen(h->str), h->str);
+	if (h->str == NULL)
+	printf("[0] (nil)\n");
+	else
+	printf("[%lu] %s\n", strlen(h->str), h->str);
 
 	h = h->next;
 	count++;
