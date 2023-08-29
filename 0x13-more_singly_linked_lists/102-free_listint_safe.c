7ba6c89c8a8b54d2718efc4ea99d4cae16e52c09
MoGracey <gmoyin53@gmail.com>
2023-08-29 14:20:36 -0700

    print safe version list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "llists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list, even if it contains a loop.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The size of the list that was freed.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *current, *temp;
+size_t count = 0;
+
+if (h == NULL || *h == NULL)
+return (0);
+
+current = *h;
+
+while (current != NULL)
+{
+count++;
+temp = current;
+current = current->next;
+
+free(temp);
+
+if (temp <= current)
+break;
+}
+
+*h = NULL;
+
+return (count);
+}
